088c0f7679526bb12676bab4b0807643f9ab93b0
Javier Ojeda <pckz@pckz.cl>
2011-12-06 13:45:55 -0300

    Arreglado la codificación y css en el panel de administración

--- admin/admin.php
+++ admin/admin.php
@@ -3,6 +3,8 @@
   <head>
     <title>Administracion OpinaICI</title>
     <link rel="stylesheet" type="text/css" href="admin.css" />
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> 
+
   </head>
 
   <body>
