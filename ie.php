584b5bc27a949e16f2385959f8edaee62e586db2
Francisco Diaz <fd@aftermath.vc>
2011-12-06 12:29:27 -0300

    Redireccion uso IE

---
+++ ie.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Infiltrado! - OpinaICI.org</title>
+<link rel="shortcut icon" type="image/vnd.microsoft.icon" href="img/favicon.ico">
+<link rel="stylesheet" type="text/css" href="ie.css" />
+</head>
+<body>
+<div id="ie-container">
+	<h1>Infiltrado!</h1>
+	<img src="img/infiltrado.png" alt="infiltrado"/>
+	<span style="font-size:18px;">Tu no estudias Ing. Civil en Inform&#225;tica.</span>
+	<span style="font-size:10px;">&#191;Si estudias? Entonces &#191;qu&#233; haces usando Internet Explorer?</span>
+</div>
+</body>
+</html>
